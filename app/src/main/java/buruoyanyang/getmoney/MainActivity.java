1033de50fbc131968fc4107156e7e472b3cf83ff
buruoyanyang <xu18716352164@gmail.com>
2016-04-19 22:56:03 +0800

    todo 使用scrollview代替rl

--- app/src/main/java/buruoyanyang/getmoney/MainActivity.java
+++ app/src/main/java/buruoyanyang/getmoney/MainActivity.java
@@ -1,13 +1,16 @@
 package buruoyanyang.getmoney;
 
 import android.os.Bundle;
+import com.jaeger.library.StatusBarUtil;
 
-public class MainActivity extends BaseActivity{
+
+public class MainActivity extends BaseActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        StatusBarUtil.setTransparent(this);
     }
 }
